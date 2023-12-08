52392e15ed85fbbc4432ed45475938915332d499
lukai404 <lukai404@163.com>
2023-12-08 21:36:27 +0800

    remove

--- App/Src/main.c
+++ App/Src/main.c
@@ -129,12 +129,15 @@ void cruise_action(){
 }
 
 
+
 void Inference_benchmark(){
     char jpg_name[30];
     DHOP_AI_IMG_Handle image;
     DH_Int32 ret = -1;
     int MAX_OUTPUT_NUM = 15;
     send_infos results;
+
+    
     for(int step = 0; step < 400 ; step++){
         if(!g_app_config.cruise_start){
             goto err0;
